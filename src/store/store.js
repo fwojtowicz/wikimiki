a9317428886716801ce09967d804f2212f712c80
Filip Wójtowicz <fil.wojtowicz@gmail.com>
2019-10-10 17:05:10 +0200

    fixed 💔

--- src/store/store.js
+++ src/store/store.js
@@ -14,7 +14,7 @@ export const store = new Vuex.Store({
         lastElement: "",
         searchURL: "https://en.wikipedia.org/w/api.php?action=query&list=allcategories&aclimit=max&format=json&acmin=1&accontinue&acprefix=",
         articlesSubcategoriesURL: "https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtype=subcat&cmlimit=500&cmtitle=",
-        articlesURL: "https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtype=page&cmlimit=500&cmtitle=Category:",
+        articlesURL: "https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtype=page&cmlimit=500&cmtitle=",
         fullURL: "",
         nextURL: "",
         categoryInput: "",
@@ -35,7 +35,8 @@ export const store = new Vuex.Store({
         password: "",
         name: "",
         currentUser: null,
-        path: ""
+        path: "",
+        randomArticlesID: []
     },
     getters: {
         searchTermGetter: state => {
@@ -162,7 +163,7 @@ export const store = new Vuex.Store({
             console.log(state.pageArray)
         },
 
-        appendSearchResultsValue(state, response) {
+        getNextPage(state, response, ) {
             state.pageStart = state.pageEnd + 1
             state.pageEnd = state.pageStart + 19;
             console.log(state.pageStart)
@@ -183,6 +184,7 @@ export const store = new Vuex.Store({
             state.pageArray = state.newArray.map(element => element)
             state.lastElement = state.categoriesArray[state.categoriesArray.length - 1].categoryCard.title;
             console.log(state.pageArray)
+
         },
 
         updateFilteredResults(state) {
@@ -195,7 +197,7 @@ export const store = new Vuex.Store({
             }
         },
 
-        chooseCategory(state, payload) {
+        chooseCategory(state, payload, ) {
             let whichTable = ""
 
             if (payload == 'selectPage') {
@@ -246,9 +248,12 @@ export const store = new Vuex.Store({
                     if (indexOfToBeDeleted !== -1) {
                         state.userCategories.splice(indexOfToBeDeleted, 1);
                     }
-                    indexOfToBeDeleted = (state.categoriesArray.findIndex(element => element.categoryCard.title === state.currentCategoryName))
-                    console.log(indexOfToBeDeleted)
-                    state.categoriesArray[indexOfToBeDeleted].categoryCard.isChosen = false
+                    // if (state.categoriesArray) {
+                    //     indexOfToBeDeleted = (state.categoriesArray.findIndex(element => element.categoryCard.title === state.currentCategoryName))
+                    //     console.log(indexOfToBeDeleted)
+                    //     state.categoriesArray[indexOfToBeDeleted].categoryCard.isChosen = false
+                    // }
+
 
                 }
 
@@ -262,6 +267,16 @@ export const store = new Vuex.Store({
                 console.log(state.userCategories)
 
             }
+        },
+
+        checkifChosen(state) {
+            console.log('just checking')
+            state.categoriesArray = state.categoriesArray.map(element => {
+                element.categoryCard.isChosen = state.userCategories.some(category => category.categoryCard.title === element.categoryCard.title);
+                return element;
+            })
+
+
         },
         getPreviousPage(state) {
             console.log(state.categoriesArray.length)
@@ -327,6 +342,12 @@ export const store = new Vuex.Store({
 
                 })
                 .catch(error => console.log(error));
+            state.categoriesArray = state.categoriesArray.map(element => {
+                element.categoryCard.isChosen = state.userCategories.some(category => category.categoryCard.title === element.categoryCard.title);
+                return element;
+            })
+            if (state.pageArray)
+                commit('checkifChosen')
         },
         getNextPageHandler: ({ commit, state }) => {
             state.dataAppended = true;
@@ -335,30 +356,35 @@ export const store = new Vuex.Store({
             axios
                 .get(state.nextURL)
                 .then(response => {
-                    commit('appendSearchResultsValue', response);
+                    commit('getNextPage', response);
                     console.log(state.lastElement);
+                    if (state.pageArray)
+                        commit('checkifChosen')
                 })
                 .catch(error => console.log(error));
+
         },
-        getRandomSubcategoriesHandler: ({ commit, state, dispatch }) => {
+        getRandomSubcategoriesHandler: ({ state, dispatch }) => {
             console.log("getting articles");
             let randomCategory = state.userCategories[Math.floor(Math.random() * state.userCategories.length)].categoryCard.title
             axios
                 .get(state.articlesSubcategoriesURL + 'Category:' + randomCategory + "&origin=*")
 
                 .then(response => {
-                    console.log(response);
                     if (response.data.query.categorymembers.length > 1) {
-                        // commit('getRandomSubcategories', response)
-                        // console.log(randomCategory);
                         let subCatsArray = response.data.query.categorymembers
-                        console.log(subCatsArray)
-                        console.log('randpmSubCat')
-                        let randomsubCat = subCatsArray[Math.floor(Math.random() * subCatsArray.length)].title
-                        console.log(randomsubCat)
+                        console.log('randomSubCat')
+                        console.log(response)
+                        let randomSubCat = subCatsArray[Math.floor(Math.random() * subCatsArray.length)].title
+                        console.log(randomSubCat)
+                        dispatch('getRandomArticlesHandler', randomSubCat)
 
                     }
-                    else if (response.data.query.categorymembers.length != 0) {
+                    else if (response.data.query.categorymembers.length == 0) {
+                        console.log('emptyArraOfSubcats')
+                        console.log(response)
+                        randomCategory = "Category:" + randomCategory
+                        console.log(randomCategory)
                         dispatch('getRandomArticlesHandler', randomCategory)
 
                     }
@@ -368,11 +394,10 @@ export const store = new Vuex.Store({
                 .catch(error => console.log(error));
         },
         getRandomArticlesHandler: ({ commit, state }, payload) => {
-            console.log('randomArticle')
-            // console.log(payload)
+            console.log('randomArticleHere')
             axios.get(state.articlesURL + payload + "&origin=*").then(response => {
                 console.log(response)
-                // console.log(response.data.query.categorymembers)
+
                 let randomArticleID = response.data.query.categorymembers[Math.floor(Math.random() * response.data.query.categorymembers.length)].pageid
                 console.log(randomArticleID)
 
@@ -382,6 +407,7 @@ export const store = new Vuex.Store({
         getPreviousPageHandler: ({ commit }) => {
             console.log("previous categoriesPage");
             commit('getPreviousPage')
+            commit('checkifChosen')
         },
         updateSearchTerm: ({ commit }, payload) => {
             console.log("updating searchTerm");
@@ -398,6 +424,7 @@ export const store = new Vuex.Store({
         setResult: ({ commit }, payload) => {
             console.log(" setResult");
             commit('setResult', payload);
+
         },
         updateCurrentCategoryID: ({ commit }, payload) => {
             commit('updateCurrentCategoryID', payload);
