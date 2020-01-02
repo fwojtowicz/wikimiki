6dce89012720a86b2f993616fc3870a441f223ae
Filip Wójtowicz <fil.wojtowicz@gmail.com>
2020-01-02 18:34:03 +0100

    WIP #2

--- src/components/pages/articlesPage/articlesPage.vue
+++ src/components/pages/articlesPage/articlesPage.vue
@@ -2,29 +2,33 @@
   <div>
     <div>
       <appHeader></appHeader>
-      <md-button
-        class="md-dense md-raised md-primary"
-        v-show="(this.$store.state.userCategories.length)!=0"
-        @click="getRandomSubcategoriesHandler"
-      >GET</md-button>
-      <md-switch v-model="showSelected" class="md-primary">Show saved articles</md-switch>
-      <div v-show="showSelected" class="md-layout md-alignment-center-center">
-        <div class="md-layout-item md-size-80">
-          <article-item
-            v-for="(userArticle, i) in userArticles"
-            :key="i"
-            :randomArticle="userArticle"
-          ></article-item>
-        </div>
-      </div>
-      <md-divider></md-divider>
       <div class="md-layout md-alignment-center-center">
         <div class="md-layout-item md-size-80">
-          <article-item
-            v-for="(randomArticle, i) in randomArticles"
-            :key="i"
-            :randomArticle="randomArticle"
-          ></article-item>
+          <md-button
+            class="md-dense md-raised md-primary"
+            v-show="(this.$store.state.userCategories.length)!=0"
+            @click="getRandomSubcategoriesHandler"
+          >GET</md-button>
+          <md-switch v-model="showSelected" class="md-primary">Show saved articles</md-switch>
+          <div v-if="showSelected">
+            <div>
+              <article-item
+                v-for="(userArticle, i) in userArticles"
+                :key="i"
+                :randomArticle="userArticle"
+              ></article-item>
+            </div>
+          </div>
+          <md-divider></md-divider>
+          <div>
+            <div class="md-layout-item md-size-80">
+              <article-item
+                v-for="(randomArticle, i) in randomArticles"
+                :key="i"
+                :randomArticle="randomArticle"
+              ></article-item>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -79,7 +83,6 @@ export default {
   }
 };
 </script>
-
 
 <style>
 </style>
